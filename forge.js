2678692bb49f590b869679ebe962c20fca55df0b
Scott Johnson <wscottjohnson5@gmail.com>
2013-08-29 23:34:16 -0400

    fixed ellipse call

--- forge.js
+++ forge.js
@@ -104,7 +104,7 @@ function draw(shape) {
 
 window.onload = function() {
     draw(new Rect(400,10,100,100));
-    draw(new Ellipse(150,100,10));
+    draw(new Ellipse(150,100,10,10));
     draw(new Line(10,10,50,50));
     draw(new Polyline([20,20,60,60,20,60,60,20]));
     draw(new Polygon([350,75,379,161,469,161,397,215,423,301,350,250,277,301,303,215,231,161,321,161]));
